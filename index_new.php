b220d860b86de518aa1a9edd317c6be4ba90c935
Chirag Jain <chiragjain0318@gmail.com>
2018-04-25 14:23:21 +0530

    Add switch case

--- index_new.php
+++ index_new.php
@@ -1,4 +1,16 @@
-<?php echo "Hello";
-echo "I am in GIT New state";
-echo "Value is set";
+<?php
+switch (n) {
+    case label1:
+        code to be executed if n=label1;
+        break;
+    case label2:
+        code to be executed if n=label2;
+        break;
+    case label3:
+        code to be executed if n=label3;
+        break;
+    ...
+    default:
+        code to be executed if n is different from all labels;
+}
 ?>
